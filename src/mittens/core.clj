028ed218718878080e5edb39172b7eda6ef7d5c7
ryan spangler <ryan.spangler@gmail.com>
2011-11-14 15:05:28 -0800

    got websockets chat working

---
+++ src/mittens/core.clj
@@ -0,0 +1,17 @@
+(ns mittens.core
+  (:require [lamina.core :as lamina]
+            [aleph.http :as http]))
+
+(def broadcast-channel (lamina/channel))
+
+(defn chat-handler [ch handshake]
+  (lamina/receive ch
+    (fn [name]
+      (lamina/siphon (lamina/map* #(str name ": " %) ch) broadcast-channel)
+      (lamina/siphon broadcast-channel ch))))
+
+(defn start []
+  (http/start-http-server chat-handler {:port 8080 :websocket true}))
+
+(defn -main []
+  (start))
